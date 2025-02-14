7f9c890c857e9e9b3ee384acb8baa2e87c5a6593
Hatice Kaplan <4haticekaplan4@gmail.com>
2025-02-14 19:51:48 +0100

    add metadata title pages

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { deleteInvoice } from '@/app/lib/actions';
+import { updateInvoice } from '@/app/lib/actions';
 export function CreateInvoice() {
   return (
     <Link
@@ -25,7 +25,8 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+ 
+  const deleteInvoiceWithId = updateInvoice.bind(null, id, {});
   return (
     <form action={deleteInvoiceWithId}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
